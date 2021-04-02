3d3911af09476d40bceb96e38ebcf66459ad54ed
LeiFu <lei@LeiFudeMacBook-Pro.local>
2021-04-02 23:21:49 +0800

    27 remove element

---
+++ 27.RemoveElement.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k++] = nums[i];
+        }
+    }
+
+    return k;
+};
+
+console.log(removeElement([3,2,2,3], 3));
